5860f1e500c2ffbbd98a9b14d7df2006b39eb021
etienne-chartrand <etiennechartrand@outlook.fr>
2022-11-16 17:15:26 -0500

    HealthBar

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    //HealthSystem
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+
     // Add the variables
     public float speed = 100f; // Speed variable
     public Rigidbody rb; // Set the variable 'rb' as Rigibody
@@ -11,22 +18,33 @@ public class CharacterMovement : MonoBehaviour
 
 
 
-    // 'Start' Method run once at start for initialisation purposes
     void Start()
     {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+
         // find the Rigidbody of this game object and add it to the variable 'rb'
         rb = this.GetComponent<Rigidbody>();
     }
 
 
-
-    // 'Update' Method is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(20);
+        }
+
         // In Update we get the Input for left, right, up and down and put it in the variable 'movement'...
         // We only get the input of x and z, y is left at 0 as it's not required
         // 'Normalized' diagonals to prevent faster movement when two inputs are used together
         movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 
 
