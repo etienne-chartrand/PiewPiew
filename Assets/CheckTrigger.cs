d1d8dc0502e812b5355eedf29ab68659613c2a7d
Hubdovakin <lefh06@uqat.ca>
2022-12-07 18:56:54 -0500

    des script pis pick up

---
+++ Assets/CheckTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckTrigger : MonoBehaviour
+{
+    
+    private GameObject player;
+    private PickUp pickUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        pickUp = player.GetComponent<PickUp>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") 
+        {
+           pickUp.inRange = true;
+            Debug.Log("j");
+        }
+    }
+}
--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+    //public float maxdistance;
+    public bool isPickedUp;
+    public bool inRange;
 
-    public Transform theDest;
-
-    void OnMouseDown()
+    public void Update()
     {
-        GetComponent<Rigidbody>().useGravity = false;
-        this.transform.position = theDest.position;
-        this.transform.parent = GameObject.Find("Destination").transform;
+        //RaycastHit hit;
+        //if(Physics.Raycast(new Vector3(transform.position.x, 0.5f, transform.position.z), new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z),out hit, maxdistance))
+        //{
+        //    Debug.DrawRay(new Vector3(transform.position.x, 0.5f, transform.position.z), new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z) * hit.distance, Color.yellow);
+        //    Debug.Log("Did Hit");
+        //}
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(inRange)
+            {
+                isPickedUp = true;
+                Debug.Log(";lkn");
+            }
+        }
+
     }
 
-    void OnMouseUp()
-    {
-        this.transform.parent = null;
-        GetComponent<Rigidbody>().useGravity = true;
-    }
+
 
 
 }
